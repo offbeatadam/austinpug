c358f844b00affc797a71cc3b859c1d38a602b26
Adam Hubscher <offbeatadam@gmail.com>
2012-02-17 18:49:38 -0600

    Grr I will get the settings right.

--- pgday/settings.py
+++ pgday/settings.py
@@ -123,7 +123,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PROJECT_PATH + '/austinpug/templates/',
+    /home/austinpug/pgday/public_html/templates/',
 )
 
 INSTALLED_APPS = (
