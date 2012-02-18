a18413516c1a31ce6e451b197c9a77218748b85d
Adam Hubscher <offbeatadam@gmail.com>
2012-02-17 18:01:26 -0600

    Added in the startup script for fcgi.

--- pgday/settings.py
+++ pgday/settings.py
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = PROJECT_PATH + '/nycpug/media/'
+MEDIA_ROOT = '/home/austinpug/pgday/public_html/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
