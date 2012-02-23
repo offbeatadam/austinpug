6aa66eeaf6c228f24c8b3e2e039542aba3458f09
Adam Hubscher <offbeatadam@gmail.com>
2012-02-23 17:44:49 -0600

    Updating the content.

--- pgday/settings.py
+++ pgday/settings.py
@@ -55,7 +55,7 @@ MEDIA_ROOT = '/home/austinpug/pgday/public_html/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://pgday.austinpug.org/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -65,7 +65,7 @@ STATIC_ROOT = '/home/austinpug/pgday/public_html/app/core/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://pgday.austinpug.org/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
